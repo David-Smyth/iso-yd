3079157f1369a15b33654406be6bbcf5dfa64277
David E. Smyth, Chief Scientist <David.Smyth.Chief.Scientist@gmail.com>
2014-01-05 21:07:58 -0800

    Initial home, about, and help.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Enable the use of Capybara to simulate browser actions
+  config.include Capybara::DSL
 end
+
