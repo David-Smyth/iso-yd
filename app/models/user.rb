bc1300ef28f7905bb9a92a770fe346e1c6178606
David E. Smyth, Chief Scientist <David.Smyth.Chief.Scientist@gmail.com>
2014-08-04 08:43:23 -0700

    Password added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+  
   validates :name, presence: true, length: { maximum: 50 }
+  
   validates :email, presence: true, format: { with: /@/ },
     uniqueness: { case_sensitive: false }
 
-    before_save { self.email.downcase! }
+  before_save { self.email.downcase! }
+
+  has_secure_password
+
+  validates :password, length: { minimum: 6 }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new name: "The Dude", email: "Dude@Abides.org" }
+  before do
+    @user = User.new name: "The Dude",
+      email: "Dude@Abides.org",
+      password: "NeverGuess",
+      password_confirmation: "NeverGuess"
+  end
   subject { @user }
 
   it { should respond_to :name }
   it { should respond_to :email }
+  # has_secure_password
+  it { should respond_to :password_digest }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+  it { should respond_to :authenticate }
+
 
   it { should be_valid }
 
@@ -71,6 +82,31 @@ describe User do
       expect( User.find_by_email(mixed_case_email.upcase)).to be_nil
       expect( User.find_by_email(mixed_case_email.downcase)).not_to be_nil
     end
+  end
+  
+  describe "Password and confirmation must match" do
+    before { @user.password_confirmation = "Nope, wrong" }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by email: @user.email }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate @user.password }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate "wrongPasswd" }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "password must be at least six characters" do
+    before { @user.password = @user.password_confirmation = "p" * 5 }
+    it { should be_invalid }
   end
 
 end
